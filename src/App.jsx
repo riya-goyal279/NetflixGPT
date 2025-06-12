ab180481f77628630319dac0be88571a441152c6
Riya Goyal <riya.goyal@axeno.co>
2025-06-12 13:24:13 +0530

    set up react app with vite and tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+      <p className="read-the-docs text-4xl text-red-400">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
